7c7cc972bb6f2a54d8cb5352655b33e2b99231cc
JacksonCampolattaro <jackson.campolattaro@gmail.com>
2026-08-18 13:21:03 +0200

    Bugfix

--- src/clouds/__init__.py
+++ src/clouds/__init__.py
@@ -1,9 +1,9 @@
 from .faust import FAUSTRemeshed as FAUSTRemeshed
 from .kitti import SemanticKITTI as SemanticKITTI
-from .kitti import SemanticWaymo as SemanticWaymo
 from .modelnet import ModelNet40 as ModelNet40
 from .modelnetc import ModelNetC as ModelNetC
 from .nuscenes import SemanticNuScenes as SemanticNuScenes
 from .s3dis import S3DIS as S3DIS
 from .scanobjectnn import ScanObjectNN as ScanObjectNN
 from .shapenetc import ShapeNetC as ShapeNetC
+from .waymo import SemanticWaymo as SemanticWaymo
