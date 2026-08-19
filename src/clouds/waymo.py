d041ec8c1e159a1e570f6b39087e74b47dd07d53
JacksonCampolattaro <jackson.campolattaro@gmail.com>
2026-08-19 08:41:52 +0200

    Save Waymo data in blocks, use multiprocessing

--- src/clouds/waymo.py
+++ src/clouds/waymo.py
@@ -7,16 +7,21 @@ import sys
 import urllib
 import zipfile
 import zlib
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import multiprocessing
 from typing import Callable, ClassVar, Union
 
 import numpy as np
 import torch
 from rich import print, progress
+from rich.progress import Progress
 from tfrecord import tfrecord_iterator
 from torch import Tensor
 from torch_geometric.data import Data, Dataset
+from torch_geometric.data.collate import collate
 from torch_geometric.data.data import BaseData, Data
 from torch_geometric.data.dataset import IndexType
+from torch_geometric.data.separate import separate
 
 
 def _get_rotation_matrix(roll, pitch, yaw):
@@ -105,6 +110,128 @@ def _range_image_to_point_image(range_image, extrinsic, inclination, pixel_pose=
     return depth_image_points
 
 
+def _convert_frames(args):
+    raw_path, test_ids = args
+    raw_split_dir, raw_filename = os.path.split(raw_path)
+    raw_dir, split = os.path.split(raw_split_dir)
+    processed_dir = os.path.join(os.path.split(raw_dir)[0], 'processed')
+    processed_filename = os.path.join(split, raw_filename.replace('.tfrecord', '.pt'))
+
+    from waymo_open_dataset.dataset_pb2 import Frame, LaserName, MatrixFloat, MatrixInt32
+
+    def _decompress_tensor(v, dtype=torch.float32) -> Tensor:
+        matrix = MatrixFloat() if dtype == torch.float32 else MatrixInt32()
+        matrix.ParseFromString(zlib.decompress(v))
+        return torch.tensor(matrix.data, dtype=dtype).view(*matrix.shape.dims)
+
+    def _frame_to_data(frame) -> Data:
+
+        # Extract relevant data from Waymo's spec
+        frame_pose_transform = torch.tensor(frame.pose.transform, dtype=torch.float32).reshape(4, 4)
+        range_images, camera_projections, label_images = dict(), dict(), dict()
+        top_pose: Tensor or None = None
+        for laser in frame.lasers:
+            assert len(laser.ri_return1.range_image_compressed) and len(laser.ri_return2.range_image_compressed)
+            range_images[laser.name] = [
+                _decompress_tensor(laser.ri_return1.range_image_compressed),
+                _decompress_tensor(laser.ri_return2.range_image_compressed),
+            ]
+            camera_projections[laser.name] = [
+                _decompress_tensor(laser.ri_return1.camera_projection_compressed, dtype=torch.int32),
+                _decompress_tensor(laser.ri_return2.camera_projection_compressed, dtype=torch.int32),
+            ]
+            if len(laser.ri_return1.segmentation_label_compressed) or len(laser.ri_return2.segmentation_label_compressed):
+                label_images[laser.name] = [
+                    _decompress_tensor(laser.ri_return1.segmentation_label_compressed, dtype=torch.int32),
+                    _decompress_tensor(laser.ri_return2.segmentation_label_compressed, dtype=torch.int32),
+                ]
+            if laser.name == LaserName.TOP:
+                top_pose = _decompress_tensor(laser.ri_return1.range_image_pose_compressed)
+
+        assert top_pose is not None
+
+        # Convert each pixel to a cartesian coordinate 
+        point_images = dict()
+        top_pose_transform = _get_transform(
+            rotation=_get_rotation_matrix(roll=top_pose[..., 0], pitch=top_pose[..., 1], yaw=top_pose[..., 2]),
+            translation=top_pose[..., 3:],
+        )
+        for c in frame.context.laser_calibrations:
+            extrinsic = torch.tensor(c.extrinsic.transform, dtype=torch.float).view(4, 4)
+            point_images[c.name] = [
+                _range_image_to_point_image(
+                    ri,
+                    extrinsic=extrinsic,
+                    inclination=(
+                        torch.tensor(c.beam_inclinations)
+                        if len(c.beam_inclinations)
+                        else _compute_inclination(c.beam_inclination_min, c.beam_inclination_max, ri.shape[0])
+                    ).flip(-1),
+                    pixel_pose=top_pose_transform if c.name == LaserName.TOP else None,
+                    frame_pose=frame_pose_transform if c.name == LaserName.TOP else None,
+                )
+                for ri in range_images[c.name]
+            ]
+            # TODO: where do normals come from?
+
+        # Flatten images
+        points, intensities, labels = [], [], []
+        for r in [0, 1]:
+            for c in frame.context.laser_calibrations:
+
+                # Identify valid pixels (e.g. sky will be ignored)
+                mask = range_images[c.name][r][..., 0] > 0
+
+                # Flatten points
+                points.append(point_images[c.name][r].reshape(-1, 3)[mask.flatten(), :])
+
+                # Extract intensity
+                intensities.append(range_images[c.name][r][..., 1].reshape(-1, 1)[mask.flatten(), :])
+
+                # Flatten labels (if present)
+                if not label_images:
+                    pass
+                elif c.name in label_images:
+                    labels.append(label_images[c.name][r].reshape(-1, 2)[mask.flatten(), :] - 1)
+                else:
+                    labels.append(torch.full((mask.sum(), 2), -1, dtype=torch.int32))
+
+                # TODO: produce normals, intensity, etc.
+
+
+        # Flatten everything into a single point cloud
+        return Data(
+            pos=torch.cat(points, dim=0),
+            intensity=torch.cat(intensities, dim=0).tanh(),
+            y=torch.cat(labels, dim=0)[:, 1] if labels else None,
+        )
+
+    frame_data_list = []
+    for raw_record in tfrecord_iterator(raw_path):
+        frame = Frame()
+        frame.ParseFromString(raw_record)
+
+        # Skip unlabeled frames, unless they're in the test set
+        if (
+            not len(frame.lasers[0].ri_return1.segmentation_label_compressed)
+            and f'{frame.context.name},{frame.timestamp_micros}' not in test_ids
+        ):
+            continue
+
+        frame_data_list.append(_frame_to_data(frame))
+
+    if not frame_data_list:
+        return []
+
+    # Save all frames from this TFRecord as a single .pt file
+    assert frame_data_list
+    data, slices, _ = collate(Data, frame_data_list, increment=False, add_batch=False)
+    torch.save((slices, data), os.path.join(processed_dir, processed_filename))
+
+    # Return the list of names & indices
+    return [(processed_filename, i) for i in range(len(frame_data_list))]
+
+
 class SemanticWaymo(Dataset):
     proto_files: ClassVar[list[str]] = [
         'waymo_open_dataset/dataset_pb2.py',
@@ -140,9 +267,16 @@ class SemanticWaymo(Dataset):
         return len(self._data)
 
     def get(self, idx: int) -> BaseData:
-        frame_id = self._data[idx]
-        data = torch.load(os.path.join(self.processed_dir, f'{frame_id}.pt'), weights_only=False)
-        return data
+        frames_file, frame_idx = self._data[idx]
+        # Load the collated data for this TFRecord
+        slices, collated_data = torch.load(os.path.join(self.processed_dir, frames_file), weights_only=False)
+        return separate(
+            cls=Data,
+            batch=collated_data,
+            idx=frame_idx,
+            slice_dict=slices,
+            decrement=False,
+        )
 
     def __getitem__(self, idx: Union[int, np.integer, IndexType]) -> Union['Dataset', BaseData]:
         if (
@@ -184,128 +318,39 @@ class SemanticWaymo(Dataset):
                 with open(os.path.join(self.raw_dir, filename), "wb") as f:
                     f.write(zf.read(filename))
 
-    def process(self) -> None:
-
-        sys.path.append(self.raw_dir)
-        from waymo_open_dataset.dataset_pb2 import Frame, LaserName, MatrixFloat, MatrixInt32
-
-        def _decompress_tensor(v, dtype=torch.float32) -> Tensor:
-            matrix = MatrixFloat() if dtype == torch.float32 else MatrixInt32()
-            matrix.ParseFromString(zlib.decompress(v))
-            return torch.tensor(matrix.data, dtype=dtype).view(*matrix.shape.dims)
-
-        def _frame_to_data(frame) -> Data:
-
-            # Extract relevant data from Waymo's spec
-            frame_pose_transform = torch.tensor(frame.pose.transform, dtype=torch.float32).reshape(4, 4)
-            range_images, camera_projections, label_images = dict(), dict(), dict()
-            top_pose: Tensor or None = None
-            for laser in frame.lasers:
-                assert len(laser.ri_return1.range_image_compressed) and len(laser.ri_return2.range_image_compressed)
-                range_images[laser.name] = [
-                    _decompress_tensor(laser.ri_return1.range_image_compressed),
-                    _decompress_tensor(laser.ri_return2.range_image_compressed),
-                ]
-                camera_projections[laser.name] = [
-                    _decompress_tensor(laser.ri_return1.camera_projection_compressed, dtype=torch.int32),
-                    _decompress_tensor(laser.ri_return2.camera_projection_compressed, dtype=torch.int32),
-                ]
-                if len(laser.ri_return1.segmentation_label_compressed) or len(laser.ri_return2.segmentation_label_compressed):
-                    label_images[laser.name] = [
-                        _decompress_tensor(laser.ri_return1.segmentation_label_compressed, dtype=torch.int32),
-                        _decompress_tensor(laser.ri_return2.segmentation_label_compressed, dtype=torch.int32),
-                    ]
-                if laser.name == LaserName.TOP:
-                    top_pose = _decompress_tensor(laser.ri_return1.range_image_pose_compressed)
-
-            assert top_pose is not None
-
-            # Convert each pixel to a cartesian coordinate 
-            point_images = dict()
-            top_pose_transform = _get_transform(
-                rotation=_get_rotation_matrix(roll=top_pose[..., 0], pitch=top_pose[..., 1], yaw=top_pose[..., 2]),
-                translation=top_pose[..., 3:],
-            )
-            for c in frame.context.laser_calibrations:
-                extrinsic = torch.tensor(c.extrinsic.transform, dtype=torch.float).view(4, 4)
-                point_images[c.name] = [
-                    _range_image_to_point_image(
-                        ri,
-                        extrinsic=extrinsic,
-                        inclination=(
-                            torch.tensor(c.beam_inclinations)
-                            if len(c.beam_inclinations)
-                            else _compute_inclination(c.beam_inclination_min, c.beam_inclination_max, ri.shape[0])
-                        ).flip(-1),
-                        pixel_pose=top_pose_transform if c.name == LaserName.TOP else None,
-                        frame_pose=frame_pose_transform if c.name == LaserName.TOP else None,
-                    )
-                    for ri in range_images[c.name]
-                ]
-                # TODO: where do normals come from?
-
-            # Flatten images
-            points, intensities, labels = [], [], []
-            for r in [0, 1]:
-                for c in frame.context.laser_calibrations:
-
-                    # Identify valid pixels (e.g. sky will be ignored)
-                    mask = range_images[c.name][r][..., 0] > 0
-
-                    # Flatten points
-                    points.append(point_images[c.name][r].reshape(-1, 3)[mask.flatten(), :])
-
-                    # Extract intensity
-                    intensities.append(range_images[c.name][r][..., 1].reshape(-1, 1)[mask.flatten(), :])
-
-                    # Flatten labels (if present)
-                    if not label_images:
-                        pass
-                    elif c.name in label_images:
-                        labels.append(label_images[c.name][r].reshape(-1, 2)[mask.flatten(), :] - 1)
-                    else:
-                        labels.append(torch.full((mask.sum(), 2), -1, dtype=torch.int32))
-
-                    # TODO: produce normals, intensity, etc.
-
-
-            # Flatten everything into a single point cloud
-            return Data(
-                pos=torch.cat(points, dim=0),
-                intensity=torch.cat(intensities, dim=0).tanh(),
-                y=torch.cat(labels, dim=0)[:, 1] if labels else None,
-            )
-
-        # Identify the test set
-        test_frame_ids = set(x.rstrip() for x in (open(self.raw_paths[-1], "r").readlines()))
-
-        # Identify raw data files
+        # Confirm all raw data files are present
         filenames = list(reversed(glob.glob(os.path.join(self.raw_dir, '*/*.tfrecord'), recursive=True)))
         assert len(filenames) == 1150
 
-        # Collect frames
+    def process(self) -> None:
+
+        sys.path.append(self.raw_dir)
+        # from waymo_open_dataset.dataset_pb2 import Frame, LaserName, MatrixFloat, MatrixInt32
+
+        # Identify the test set
+        test_frame_ids = set(x.rstrip() for x in (open(self.raw_paths[-1], "r").readlines()))
+        
+        # Iterate over splits
         split_ids = dict(training=[], validation=[], testing=[])
-        for f in progress.track(filenames, description="Loading Waymo data", show_speed=True):
-            split = os.path.split(os.path.split(f)[0])[-1]
-            for raw_record in tfrecord_iterator(f):
-                frame = Frame()
-                frame.ParseFromString(raw_record)
+        for split in split_ids:
 
-                # Skip unlabeled frames, unless they're in the test set
-                frame_id = f'{frame.context.name},{frame.timestamp_micros}'
-                if not len(frame.lasers[0].ri_return1.segmentation_label_compressed) and frame_id not in test_frame_ids:
-                    continue
+            # Create processed split directory
+            os.makedirs(os.path.join(self.processed_dir, split), exist_ok=True)
+            
+            # Glob all tfrecord files in this split
+            filenames = glob.glob(os.path.join(self.raw_dir, split, '*.tfrecord'))
+            with (
+                Progress(*Progress.get_default_columns(), progress.TimeElapsedColumn()) as p,
+                multiprocessing.Pool(processes=8) as pool,
+            ):
+                task = p.add_task(f"Loading Waymo {split} data", total=len(filenames))
+                p.update(task, advance=0)
+                for result in pool.imap_unordered(_convert_frames, [(f, test_frame_ids) for f in filenames]):
+                    split_ids[split].extend(result)
+                    p.update(task, advance=1)
 
-                data = _frame_to_data(frame)
-
-                if self.pre_filter is not None and not self.pre_filter(data):
-                    continue
-
-                split_ids[split].append(frame_id)
-                torch.save(data, os.path.join(self.processed_dir, f'{frame_id}.pt'))
-
+        # Save slices for each filename
         torch.save(split_ids, os.path.join(self.processed_dir, 'splits.pt'))
-
 
     @property
     def raw_file_names(self) -> list[str]:
@@ -325,17 +370,16 @@ if __name__ == '__main__':
     root = os.path.join(os.path.realpath(sys.argv[1]), 'SemanticWaymo')
     print(root)
     dataset = SemanticWaymo(root=root, split='val')
-    for i, data in enumerate(dataset):
+    for data in progress.track(dataset):
+        print(data)
+    # for i, data in enumerate(dataset):
 
-        import polyscope
-        polyscope.init()
-        polyscope.set_up_dir('z_up')
-        c = polyscope.register_point_cloud('waymo', data.pos)
-        c.add_scalar_quantity('y', data.y)
-        c.add_scalar_quantity('intensity', data.intensity.flatten())
-        polyscope.show()
+    #     # import polyscope
+    #     # polyscope.init()
+    #     # polyscope.set_up_dir('z_up')
+    #     # c = polyscope.register_point_cloud('waymo', data.pos)
+    #     # c.add_scalar_quantity('y', data.y)
+    #     # c.add_scalar_quantity('intensity', data.intensity.flatten())
+    #     # polyscope.show()
 
-        print(i, data)
-
-
-
+    #     print(i, data)
