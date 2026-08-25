baff3c89b3d0e6240b26e8cab3b99d8bc2724fa6
JacksonCampolattaro <jackson.campolattaro@gmail.com>
2026-08-25 09:22:11 +0200

    Mix3D now makes a copy rather than modifying the batch directly

--- src/clouds/transforms/mix3d.py
+++ src/clouds/transforms/mix3d.py
@@ -13,27 +13,38 @@ class Mix3D(BaseTransform):
 
     def forward(self, data: Data) -> Data:
         assert isinstance(data.ptr, Tensor)
+        batch_size = data.ptr.size(0) - 1
 
-        # Boundaries between items will be deleted with probability p
-        data.ptr = data.ptr[
+        # Boundaries between items will be deleted with probability 
+        new_ptr = data.ptr[
             torch.cat(
                 [
                     data.ptr.new_ones(1, dtype=torch.bool),
-                    self._boundary_mask(data.ptr.size(0) - 2, data.ptr.device),
+                    self._boundary_mask(batch_size - 1, data.ptr.device),
                     data.ptr.new_ones(1, dtype=torch.bool),
                 ]
             )
         ]
 
-        # Replace data.batch to match new data.ptr
-        data.batch = torch.repeat_interleave(
-            torch.arange(data.ptr.size(0) - 1, device=data.ptr.device),
-            data.ptr[1:] - data.ptr[:-1],
-        )
+        # Determine which batches are merged-into
+        kept_batches = data.batch[new_ptr[:-1]]
 
-        data.batch_size = data.ptr.size(0)
+        # Copy data into a new Data object
+        out = type(data)()
+        for key, item in data.items():
+            if key == 'ptr':
+                out[key] = new_ptr
+            elif key == 'batch':
+                out[key] = torch.repeat_interleave(
+                    torch.arange(new_ptr.size(0) - 1, device=new_ptr.device),
+                    new_ptr[1:] - new_ptr[:-1],
+                )
+            elif item.size(0) == batch_size:
+                out[key] = item[kept_batches]
+            else:
+                out[key] = item
 
-        return data
+        return out
 
     def _boundary_mask(self, n: int, device: torch.device) -> Tensor:
         """Sample which interior boundaries to delete, never deleting
