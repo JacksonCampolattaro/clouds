fbf1eb9d390d6e940c1dd4b74d45551845c36c0a
JacksonCampolattaro <jackson.campolattaro@gmail.com>
2026-09-01 14:12:05 +0200

    Adapt auto contrast to batched data

--- src/clouds/transforms/color.py
+++ src/clouds/transforms/color.py
@@ -1,7 +1,9 @@
 import random
 
+import torch
 from torch import Tensor
 from torch_geometric.data import Data
+from torch_geometric.nn.aggr import MaxAggregation, MinAggregation
 from torch_geometric.transforms import BaseTransform
 
 
@@ -11,18 +13,17 @@ class RandomColorAutoContrast(BaseTransform):
         self.blend_factor = blend_factor
 
     def forward(self, data: Data) -> Data:
-        # TODO: support batches?
-        assert not isinstance(data.batch, Tensor)
-
-        if random.random() > self.p:
+        if not isinstance(data.batch, Tensor) and random.random() > self.p:
             return data
 
-        colmin = data.color.amin(dim=0, keepdim=True)
-        colmax = data.color.amax(dim=0, keepdim=True)
+        colmin = MinAggregation()(data.color, index=data.batch, ptr=getattr(data, 'ptr', None), dim=0)
+        colmax = MaxAggregation()(data.color, index=data.batch, ptr=getattr(data, 'ptr', None), dim=0)
         scale = 1 / (1e-7 + colmax - colmin)
-        alpha = self.blend_factor or random.random()
-        data.color = (1 - alpha + alpha * scale) * data.color - alpha * colmin * scale
+        alpha = self.blend_factor or torch.rand_like(scale)
+        if isinstance(data.batch, Tensor):
+            scale, alpha = scale[data.batch], alpha[data.batch]
 
+        data.color = (1 - alpha + alpha * scale) * data.color - alpha * colmin * scale
         return data
 
     def __repr__(self) -> str:
