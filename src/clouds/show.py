d9e0e91c076181175fc1dbaff6be9c4876ae3a54
JacksonCampolattaro <jackson.campolattaro@gmail.com>
2026-09-04 11:34:37 +0200

    Fix bug showing global properties of un-batched data

--- src/clouds/show.py
+++ src/clouds/show.py
@@ -152,6 +152,8 @@ def register_nodes(
         # Broadcast instance-global features to all points
         if hasattr(nodes, 'batch') and item.size(0) == (nodes.batch.amax() + 1):
             item = item[nodes.batch]
+        elif item.size(0) == 1:
+            item = item.unsqueeze(0).repeat(num_nodes)
 
         # Convert selection indices to masks
         # FIXME: maybe remove this?
